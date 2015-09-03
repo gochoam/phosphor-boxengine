e1068ad3e54c1c8783d6bf556f3f3d20a0707bc4
S. Chris Colbert <sccolbert@gmail.com>
2015-09-03 16:42:24 -0400

    update to TS 1.6

--- scripts/dtsbundle.js
+++
@@ -1,7 +0,0 @@
-require('dts-generator').generate({
-  name: 'phosphor-boxengine',
-  main: 'phosphor-boxengine/index',
-  baseDir: 'lib',
-  files: ['index.d.ts'],
-  out: 'lib/phosphor-boxengine.d.ts',
-});
